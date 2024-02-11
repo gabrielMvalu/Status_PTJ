3c021043968782d5bdaabf21e6eefd0347de18bd
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-11 19:51:23 +0200

    Update Brenado_for_House.py

--- Brenado_for_House.py
+++ Brenado_for_House.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 # Setări inițiale
 titlu_proiect = "Numele Proiectului"
 descriere_proiect = "Acesta este un scurt rezumat al proiectului."
-url_github = "https://github.com/username/proiect"
+url_github = "https://castemill.com"
 data_inceput = datetime(2024, 2, 9)
 data_limita = datetime(2024, 3, 20)
 data_curenta = datetime.now()
@@ -15,7 +15,7 @@ zile_trecute = (data_curenta - data_inceput).days
 progres = min(max(zile_trecute / total_zile_proiect, 0), 1)  # Asigură că progresul este între 0 și 1
 
 # Construirea paginii Streamlit
-st.title(titlu_proiect)
+st.title(':blue[{titlu_proiect}]')
 st.write(descriere_proiect)
 st.progress(progres)
 
